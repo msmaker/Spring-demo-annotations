b44e8a9b62fd5516bb056bef7ecccc0cca12c5c4
Marco Silva <marcosilva.tec@gamil.com>
2021-06-01 16:18:22 -0300

    Spring Configuration with Java Annotations

---
+++ src/com/msmaker/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.msmaker.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.msmaker.springdemo")
+public class SportConfig {
+
+}
