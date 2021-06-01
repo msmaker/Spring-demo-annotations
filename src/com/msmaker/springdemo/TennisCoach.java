2a90c0b6bb671d6be8f1f5917e72d34b029af918
Marco Silva <marcosilva.tec@gamil.com>
2021-06-01 12:42:10 -0300

    Qualifier

--- src/com/msmaker/springdemo/TennisCoach.java
+++ src/com/msmaker/springdemo/TennisCoach.java
@@ -1,12 +1,14 @@
 package com.msmaker.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
 	@Autowired
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 
 	// define a default constructor
@@ -15,11 +17,9 @@ public class TennisCoach implements Coach {
 	}
 
 	/*
-	@Autowired
-	public void setFortuneService(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;
-	}
-*/
+	 * @Autowired public void setFortuneService(FortuneService theFortuneService) {
+	 * fortuneService = theFortuneService; }
+	 */
 	/*
 	 * @Autowired public TennisCoach(FortuneService theFortuneService) {
 	 * fortuneService = theFortuneService; }
